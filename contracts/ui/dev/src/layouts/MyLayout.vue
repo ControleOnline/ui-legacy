399fb8168ef971baa03986b794eead60b7bfac9b
Francisco Rivera <franciscoriverap@gmail.com>
2021-06-04 16:48:11 -0300

    package.json

--- contracts/ui/dev/src/layouts/MyLayout.vue
+++ contracts/ui/dev/src/layouts/MyLayout.vue
@@ -11,7 +11,7 @@
         />
 
         <q-toolbar-title>
-          @conline/quasar-contracts-ui v{{ version }}
+          @controleonline/quasar-contracts-ui v{{ version }}
         </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
