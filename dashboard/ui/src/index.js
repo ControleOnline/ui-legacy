8e4fd07a785b41276307d98979aa623953496c7e
Guilherme Smart Web <guilherme@smartwebsistemas.com.br>
2021-07-08 10:36:34 -0300

    new dashboard metrics

--- dashboard/ui/src/index.js
+++ dashboard/ui/src/index.js
@@ -1,73 +1,81 @@
-import { name, version }      from '../package.json'
+import { name, version }      from '../package.json';
 
-import DashboardDefault       from './components/DashboardDefault'
-import InactiveCustomers      from './components/InactiveCustomers'
-import ActiveCustomers        from './components/ActiveCustomers'
-import NewCustomers           from './components/NewCustomers'
-import ProspectiveCustomers   from './components/ProspectiveCustomers'
-import QuoteOrderTotal        from './components/QuoteOrderTotal'
-import SalesOrderTotal        from './components/SalesOrderTotal'
-import AverageTicket          from './components/AverageTicket'
-import ComissionOrderTotal    from './components/ComissionOrderTotal'
-import OperationalExpenses    from './components/OperationalExpenses'
-import AdministrativeExpenses from './components/AdministrativeExpenses'
-import ActiveContracts        from './components/ActiveContracts'
-import ChartSalesMoney        from './components/ChartSalesMoney'
-import ChartSalesOrder        from './components/ChartSalesOrder'
+import DashboardDefault       from './components/DashboardDefault';
+import InactiveCustomers      from './components/InactiveCustomers';
+import ActiveCustomers        from './components/ActiveCustomers';
+import NewCustomers           from './components/NewCustomers';
+import ProspectiveCustomers   from './components/ProspectiveCustomers';
+import QuoteOrderTotal        from './components/QuoteOrderTotal';
+import SalesOrderTotal        from './components/SalesOrderTotal';
+import AverageTicket          from './components/AverageTicket';
+import ComissionOrderTotal    from './components/ComissionOrderTotal';
+import OperationalExpenses    from './components/OperationalExpenses';
+import AdministrativeExpenses from './components/AdministrativeExpenses';
+import ActiveContracts        from './components/ActiveContracts';
+import ChartSalesMoney        from './components/ChartSalesMoney';
+import ChartSalesOrder        from './components/ChartSalesOrder';
+import Enrollment             from './components/Enrollment';
+import NewContracts           from './components/NewContracts';
 
 export {
   name,
   version,
 
-  DashboardDefault    ,
-  InactiveCustomers   ,
-  ActiveCustomers     ,
-  NewCustomers        ,
-  ProspectiveCustomers,
-  QuoteOrderTotal     ,
-  SalesOrderTotal     ,
-  AverageTicket       ,
-  ComissionOrderTotal ,
-  OperationalExpenses ,
+  DashboardDefault      ,
+  InactiveCustomers     ,
+  ActiveCustomers       ,
+  NewCustomers          ,
+  ProspectiveCustomers  ,
+  QuoteOrderTotal       ,
+  SalesOrderTotal       ,
+  AverageTicket         ,
+  ComissionOrderTotal   ,
+  OperationalExpenses   ,
   AdministrativeExpenses,
-  ActiveContracts,
-  ChartSalesMoney,
-  ChartSalesOrder,
+  ActiveContracts       ,
+  ChartSalesMoney       ,
+  ChartSalesOrder       ,
+  Enrollment            ,
+  NewContracts          ,
 }
 
 export default {
   name,
   version,
 
-  DashboardDefault    ,
-  InactiveCustomers   ,
-  ActiveCustomers     ,
-  NewCustomers        ,
-  ProspectiveCustomers,
-  QuoteOrderTotal     ,
-  SalesOrderTotal     ,
-  AverageTicket       ,
-  ComissionOrderTotal ,
-  OperationalExpenses ,
+  DashboardDefault      ,
+  InactiveCustomers     ,
+  ActiveCustomers       ,
+  NewCustomers          ,
+  ProspectiveCustomers  ,
+  QuoteOrderTotal       ,
+  SalesOrderTotal       ,
+  AverageTicket         ,
+  ComissionOrderTotal   ,
+  OperationalExpenses   ,
   AdministrativeExpenses,
-  ActiveContracts,
-  ChartSalesMoney,
-  ChartSalesOrder,
+  ActiveContracts       ,
+  ChartSalesMoney       ,
+  ChartSalesOrder       ,
+  Enrollment            ,
+  NewContracts          ,
 
   install (Vue) {
-    Vue.component(DashboardDefault.name    , DashboardDefault )
-    Vue.component(InactiveCustomers.name   , InactiveCustomers)
-    Vue.component(ActiveCustomers.name     , ActiveCustomers)
-    Vue.component(NewCustomers.name        , NewCustomers)
-    Vue.component(ProspectiveCustomers.name, ProspectiveCustomers)
-    Vue.component(QuoteOrderTotal.name     , QuoteOrderTotal)
-    Vue.component(SalesOrderTotal.name     , SalesOrderTotal)
-    Vue.component(AverageTicket.name       , AverageTicket)
-    Vue.component(ComissionOrderTotal.name , ComissionOrderTotal)
-    Vue.component(OperationalExpenses.name , OperationalExpenses)
+    Vue.component(DashboardDefault.name      , DashboardDefault)
+    Vue.component(InactiveCustomers.name     , InactiveCustomers)
+    Vue.component(ActiveCustomers.name       , ActiveCustomers)
+    Vue.component(NewCustomers.name          , NewCustomers)
+    Vue.component(ProspectiveCustomers.name  , ProspectiveCustomers)
+    Vue.component(QuoteOrderTotal.name       , QuoteOrderTotal)
+    Vue.component(SalesOrderTotal.name       , SalesOrderTotal)
+    Vue.component(AverageTicket.name         , AverageTicket)
+    Vue.component(ComissionOrderTotal.name   , ComissionOrderTotal)
+    Vue.component(OperationalExpenses.name   , OperationalExpenses)
     Vue.component(AdministrativeExpenses.name, AdministrativeExpenses)
-    Vue.component(ActiveContracts.name, ActiveContracts)
-    Vue.component(ChartSalesMoney.name, ChartSalesMoney)
-    Vue.component(ChartSalesOrder.name, ChartSalesOrder)
+    Vue.component(ActiveContracts.name       , ActiveContracts)
+    Vue.component(ChartSalesMoney.name       , ChartSalesMoney)
+    Vue.component(ChartSalesOrder.name       , ChartSalesOrder)
+    Vue.component(Enrollment.name            , Enrollment)
+    Vue.component(NewContracts.name          , NewContracts)
   }
 }
