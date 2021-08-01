ad4ba0f19a5df1f2a906372666f50b196812d09c
Luiz Kim <luiz.kim@controleonline.com>
2021-08-01 12:13:29 -0300

    Add customers

---
+++ contracts/ui/src/router/routes.js
@@ -0,0 +1,10 @@
+export const routes = [
+  {
+    path: '/contracts/',
+    component: () => import('layouts/AdminLayout.vue'),
+    children: [
+      { name: 'ContractIndex'  , path: ''      , component: () => import('pages/Contracts/Index.vue'  ) },
+      { name: 'ContractDetails', path: 'id/:id', component: () => import('pages/Contracts/Details.vue') },
+    ]
+  }
+];
