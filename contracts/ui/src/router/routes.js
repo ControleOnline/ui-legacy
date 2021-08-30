f9219ee2aaf3147ebf187ea60e42a7cd77f6ef57
Luiz Kim <luiz.kim@controleonline.com>
2021-08-29 21:05:27 -0300

    Contract

--- contracts/ui/src/router/routes.js
+++ contracts/ui/src/router/routes.js
@@ -17,7 +17,7 @@ export const routes = [
   },
   {
     path: '/contract/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('../../../../../src/layouts/MainLayout.vue'),
     children: [      
       { name: 'ContractAccept', path: 'accept/:id', component: () => import('../pages/Contracts/Accept.vue') },      
     ]
