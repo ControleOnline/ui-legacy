8e636df0ba9dc04438f9fe02b127ecbaa003837e
Luiz Kim <luiz.kim@controleonline.com>
2021-09-21 16:34:56 -0300

    Update

--- common/ui/src/store/config/actions.js
+++ common/ui/src/store/config/actions.js
@@ -3,46 +3,72 @@ import fetch           from '@controleonline/quasar-common-ui/src/utils/fetch';
 import * as types      from './mutation_types';
 
 export const appConfig = ({ commit }) => {
-  commit(types.SET_ISLOADING);
+  commit(types.SET_ISLOADING)
 
-  let params   = {
-      method: 'GET',
-    };
+  const params = {
+    method: 'GET'
+  }
 
   return fetch('/configs/app-config', params)
     .then(response => response.json())
     .then(data => {
+      commit(types.SET_ISLOADING, false)
 
-      commit(types.SET_ISLOADING, false);
-
-      let config = {
-        gtmId: null,
-      };
+      const config = {
+        gtmId: null
+      }
 
       if (data.response.success) {
-
         // Google Tag Manager ID
 
         if (data.response.data.gtmId) {
-          config.gtmId = JSON.parse(data.response.data.gtmId);
-          config.gtmId = config.gtmId[0];
+          config.gtmId = JSON.parse(data.response.data.gtmId)
+          config.gtmId = config.gtmId[0]
         }
       }
 
-      commit(types.SET_APPCONFIG, config);
+      commit(types.SET_APPCONFIG, config)
 
-      return config;
-
+      return config
     }).catch(e => {
-      commit(types.SET_ISLOADING, false);
+      commit(types.SET_ISLOADING, false)
 
       if (e instanceof SubmissionError) {
-        commit(types.SET_VIOLATIONS, e.errors);
+        commit(types.SET_VIOLATIONS, e.errors)
         // eslint-disable-next-line
         commit(types.SET_ERROR, e.errors._error);
-        return;
+        return
       }
 
-      commit(types.SET_ERROR, e.message);
+      commit(types.SET_ERROR, e.message)
+    })
+}
+
+export const getConfig = ({ commit }, peopleId) => {
+  const params = {
+    method: 'GET'
+  };
+
+  return myapi.fetch(`/configs/${peopleId}`, params)
+    .then(response => response.json())
+    .then(response => {
+
+      return response.response ? response.response.data : null;
+
+    });
+}
+
+export const saveConfig = ({ commit }, { peopleId, values, params }) => {
+  let options = {
+    method: 'PUT',
+    body  : JSON.stringify(values),
+    params: params
+  };
+
+  return myapi.fetch(`/configs/${peopleId}`, options)
+    .then(response => response.json())
+    .then(response => {
+      return response;
     });
 };
+
