9ad6ef360a08bc7d1e9caee0233f1286ed1bdc01
Luiz Kim <luiz.kim@controleonline.com>
2021-09-23 13:32:53 -0300

    Routes

--- users/ui/src/router/routes.js
+++ users/ui/src/router/routes.js
@@ -11,10 +11,10 @@ export const routes = [
     component: () => import('../../../../../src/layouts/AdminLayout.vue'),
     children: [
       {
-        path     : ':id/employees/:employeeId',
+        path     : 'id/:id/employees/:employeeId',
         name     : 'CompanyEmployee',
         component: () => import('../pages/admin/Company/Employee.vue')
-      },      
+      },
       {
         name: 'CompanyIndex',
         path: '',
