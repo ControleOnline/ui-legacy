064e2aeff69284f8b148ef43d58e1ce4277f9d45
Luiz Kim <luiz.kim@controleonline.com>
2021-10-04 11:32:19 -0300

    Translate

---
+++ financial/ui/src/i18n/pt-br/index.js
@@ -0,0 +1,19 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export const translate = {
+  invoice: {
+    statuses: {
+      'waiting payment': 'Aguardando pagamento',
+      'paid': 'Pago',
+      'divergence of values': 'Divergência de valores',
+      'canceled': 'Cancelado',
+      'outdated billing': 'Fatura em atraso',
+      'open': 'Aberto',
+      'exceeded billing': 'Limite de faturamento excedido',
+      'waiting billing': 'Aguardando fatura',
+      'waiting generate invoice': 'Gerando NF',
+    },
+  },
+  
+}
