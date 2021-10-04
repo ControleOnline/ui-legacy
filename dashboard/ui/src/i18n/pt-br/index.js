ca2d1316508741505babb5a2208c8499ee5dff53
Luiz Kim <luiz.kim@controleonline.com>
2021-10-04 10:55:48 -0300

    Translate

--- dashboard/ui/src/i18n/pt-br/index.js
+++ dashboard/ui/src/i18n/pt-br/index.js
@@ -1,7 +1,7 @@
 // This is just an example,
 // so you can safely delete all default props below
 
-export default {
+export const translate = {
   dashboard: {
     from_date              : 'Data inicio',
     to_date                : 'Data fim',
@@ -23,14 +23,5 @@ export default {
       sales_orders   : 'Pedidos de venda',
       sales_order_qty: 'Vendas por quantidade de pedidos',
     },
-  },
-  messages         : {
-    saved_successfully: 'Dados salvos com sucesso',
-    select_an_option  : 'Selecione uma opção',
-    field_required    : 'Este campo é obrigatório',
-    email_invalid     : 'O email informado não é válido',
-    value_invalid     : 'O valor informado não é válido',
-    select_a_date     : 'Selecione uma data',
-    sure_remove       : 'Realmente deseja eliminar este registro?',
   },
 }
