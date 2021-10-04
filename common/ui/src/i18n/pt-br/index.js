1fb128265144ae489bb3792df1d3dea5b8701193
Luiz Kim <luiz.kim@controleonline.com>
2021-10-04 11:31:08 -0300

    Translate

---
+++ common/ui/src/i18n/pt-br/index.js
@@ -0,0 +1,21 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export const translate = {
+  common: {
+    labels: {
+      close: 'Fechar',
+    }
+  },
+  menu: {
+    carriers: 'Bases'
+  },
+  messages: {
+    fieldRequired: 'Este campo é obrigatório',
+    emailInvalid: 'O email informado não é válido',
+    phoneInvalid: 'O fone informado não é válido',
+    anErrorOccurred: 'Não foi possível atender a solicitação',
+    gmapsReqNoData: 'Pesquisa sem resultados',
+    mustDefinePackages: 'Deve definir a cubagem total ou os productos'
+  },
+}
