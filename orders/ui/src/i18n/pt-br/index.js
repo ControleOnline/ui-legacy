9082fa9ccf8343f7485b942275d25853f94ab589
Luiz Kim <luiz.kim@controleonline.com>
2021-10-04 11:32:47 -0300

    Translate

---
+++ orders/ui/src/i18n/pt-br/index.js
@@ -0,0 +1,28 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export const translate = {
+  order: {
+    inMyCompany: 'Base ou Ponto de Encontro',
+    inOtherPlace: 'Coleta por Guincho',
+    inOtherPlaceDelivery: 'Entrega por Guincho',
+    statuses: {
+      'quote': 'Cotação',
+      'waiting client invoice tax': 'Aguardando nota fiscal',
+      'automatic analysis': 'Análise automática',
+      'analysis': 'Em análise',
+      'waiting payment': 'Aguardando pagamento',
+      'waiting retrieve': 'Aguardando coleta',
+      'on the way': 'Em viagem',
+      'waiting invoice tax': 'Aguardando fatura',
+      'delivered': 'Entregue',
+      'waiting billing': 'Gerando NF',
+      'canceled': 'Cancelado',
+      'waiting commission': 'Aguardando comissão',
+      'ship to carrier': 'Entregar na transportadora',
+      'retrieved': 'Coletado',
+      'entered the base': "Entrou na Base",
+      'boarded': "Embarcou"
+    },
+  },
+}
