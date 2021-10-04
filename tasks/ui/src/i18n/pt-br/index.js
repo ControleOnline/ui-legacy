dd228ce9de175e19e4b42d936d5ea60a6addcc81
Luiz Kim <luiz.kim@controleonline.com>
2021-10-04 11:33:55 -0300

    Translate

--- tasks/ui/src/i18n/pt-br/index.js
+++ tasks/ui/src/i18n/pt-br/index.js
@@ -1,7 +1,7 @@
 // This is just an example,
 // so you can safely delete all default props below
 
-export default {
+export const translate = {
   tasks: {
     create        : 'Tarefas Criadas',
     myTasks       : 'Minhas Tarefas',
