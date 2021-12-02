695197c71119e8fc3c2a6c8963de165dc8eeceb2
Luiz Kim <luiz.kim@controleonline.com>
2021-12-02 13:34:19 -0300

    Update

--- orders/ui/src/router/routes.js
+++ orders/ui/src/router/routes.js
@@ -1,34 +1,50 @@
 
 export const routes = [
     {
-        path     : '/sales/',
+        path: '/sales/',
         component: () => import('../../../../../src/layouts/AdminLayout.vue'),
-        children : [
-            { 
-                name: 'SalesOrderIndex', 
-                path: 'order'          , 
-                component: () => import('../pages/Orders/Sales/Index.vue') 
+        children: [
+            {
+                name: 'SalesOrderIndex',
+                path: 'order',
+                component: () => import('../pages/Orders/Sales/Index.vue')
             },
-            { 
-                name: 'OrderDetails', 
-                path: 'order/id/:id', 
-                component: () => import('../pages/Orders/Sales/Order.vue') 
+            {
+                name: 'OrderDetails',
+                path: 'order/id/:id',
+                component: () => import('../pages/Orders/Sales/Order.vue')
             },
         ]
     },
     {
-        path     : '/purchasing/',
+        path: '/client/',
         component: () => import('../../../../../src/layouts/AdminLayout.vue'),
-        children : [
-            { 
-                name: 'PurchasingOrderIndex', 
-                path: 'order'               , 
-                component: () => import('../pages/Orders/Purchasing/Index.vue') 
+        children: [
+            {
+                name: 'ClientOrderIndex',
+                path: 'order',
+                component: () => import('../pages/Orders/Client/Index.vue')
             },
-            { 
-                name: 'PurchasingOrderDetails', 
-                path: 'order/id/:id'          , 
-                component: () => import('../pages/Orders/Purchasing/Order.vue') 
+            {
+                name: 'ClientOrderDetails',
+                path: 'order/id/:id',
+                component: () => import('../pages/Orders/Client/Order.vue')
+            },
+        ]
+    },
+    {
+        path: '/purchasing/',
+        component: () => import('../../../../../src/layouts/AdminLayout.vue'),
+        children: [
+            {
+                name: 'PurchasingOrderIndex',
+                path: 'order',
+                component: () => import('../pages/Orders/Purchasing/Index.vue')
+            },
+            {
+                name: 'PurchasingOrderDetails',
+                path: 'order/id/:id',
+                component: () => import('../pages/Orders/Purchasing/Order.vue')
             },
         ]
     },
