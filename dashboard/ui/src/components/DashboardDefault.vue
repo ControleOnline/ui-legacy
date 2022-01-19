897c95b0f56cbbbb889ff974b54d5cef23c7b336
Luiz Kim <luiz.kim@controleonline.com>
2022-01-19 13:54:09 -0300

    Update

--- dashboard/ui/src/components/DashboardDefault.vue
+++ dashboard/ui/src/components/DashboardDefault.vue
@@ -1,68 +1,20 @@
 <template>
   <div>
-    <q-card class="q-mb-sm">
-      <q-card-section>
-        <div class="row">
-          <div class="col-xs-12 col-sm-6 q-pa-sm">
-            <q-input stack-label
-              v-model="fromDate"
-              :label ="$t('dashboard.from_date')"
-              mask   ="##/##/####"
-            >
-              <template v-slot:append>
-                <q-icon name="event" class="cursor-pointer">
-                  <q-popup-proxy ref="qDateProxy1" transition-show="scale" transition-hide="scale">
-                    <q-date
-                      v-model ="fromDate"
-                      mask    ="DD/MM/YYYY"
-                      @input  ="() => $refs.qDateProxy1.hide()"
-                    />
-                  </q-popup-proxy>
-                </q-icon>
-              </template>
-            </q-input>
-          </div>
-          <div class="col-xs-12 col-sm-6 q-pa-sm">
-            <q-input stack-label
-              v-model="toDate"
-              :label ="$t('dashboard.to_date')"
-              mask   ="##/##/####"
-            >
-              <template v-slot:append>
-                <q-icon name="event" class="cursor-pointer">
-                  <q-popup-proxy ref="qDateProxy2" transition-show="scale" transition-hide="scale">
-                    <q-date
-                      v-model ="toDate"
-                      mask    ="DD/MM/YYYY"
-                      @input  ="() => $refs.qDateProxy2.hide()"
-                    />
-                  </q-popup-proxy>
-                </q-icon>
-              </template>
-            </q-input>
-          </div>
-        </div>
-      </q-card-section>
-      <q-card-actions align="center">
-        <q-btn flat
-          :loading="isLoading"
-          :label  ="$t('dashboard.request')"
-          color   ="primary"
-          @click  ="$emit('refresh')"
-        />
-      </q-card-actions>
-    </q-card>
-
+    <DataFilter
+      :fromDate="fromDate"
+      :toDate="toDate"
+      @dateChanged="dateChanged"
+    />
     <q-card class="q-pa-md q-mb-sm">
       <div class="row q-col-gutter-sm">
         <div
           class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-xs-12"
           v-for="element in elements"
-          :key ="element.name"
+          :key="element.name"
         >
           <slot
-            :name         ="element.name"
-            v-bind:config ="config"
+            :name="element.name"
+            v-bind:config="config"
             v-bind:filters="filters"
           >
             {{ `Dashboard element '${element.name}' not found` }}
@@ -71,16 +23,11 @@
       </div>
     </q-card>
 
-    <q-card v-if="charts.length > 0"
-      class="q-pa-md q-mb-sm"
-    >
-      <q-card-section
-        v-for="chart in charts"
-        :key ="chart.name"
-      >
+    <q-card v-if="charts.length > 0" class="q-pa-md q-mb-sm">
+      <q-card-section v-for="chart in charts" :key="chart.name">
         <slot
-          :name         ="chart.name"
-          v-bind:config ="config"
+          :name="chart.name"
+          v-bind:config="config"
           v-bind:filters="filters"
         >
           {{ `Dashboard chart '${chart.name}' not found` }}
@@ -91,48 +38,58 @@
 </template>
 
 <script>
-import configurable from './../mixins/configurable';
-import { date }     from 'quasar';
+import DataFilter from "@controleonline/quasar-common-ui/src/components/common/DataFilter.vue";
+import configurable from "./../mixins/configurable";
+import { date } from "quasar";
 
 export default {
-  name  : 'DashboardDefault',
-  mixins: [ configurable ],
+  components: {
+    DataFilter,
+  },
+  name: "DashboardDefault",
+  mixins: [configurable],
 
-  props : {
+  props: {
     elements: {
-      type    : Array,
+      type: Array,
       required: true,
     },
-    charts  : {
-      type    : Array,
+    charts: {
+      type: Array,
       required: false,
-      default : function() {
+      default: function () {
         return [];
-      }
+      },
     },
   },
-
+  methods: {
+    dateChanged() {
+      this.$emit("refresh");
+    },
+  },
   computed: {
     filters() {
       return {
         from: this.fromDate,
-        to  : this.toDate,
-      }
+        to: this.toDate,
+      };
     },
   },
 
   data() {
     return {
-      fromDate : date.formatDate(date.subtractFromDate(Date.now(), { month: 1 }), 'DD/MM/YYYY'),
-      toDate   : date.formatDate(Date.now(), 'DD/MM/YYYY'),
+      fromDate: date.formatDate(
+        date.subtractFromDate(Date.now(), { month: 1 }),
+        "DD/MM/YYYY"
+      ),
+      toDate: date.formatDate(Date.now(), "DD/MM/YYYY"),
       isLoading: false,
-    }
+    };
   },
 };
 </script>
 
 <style lang="sass" scoped>
-  .my-card
-    min-height: 100%
-
+.my-card
+  min-height: 100%
 </style>
