acc83fa5f53d827e8b7ab9043503f1945a1277cf
Luiz Kim <luiz.kim@controleonline.com>
2022-03-03 14:05:25 -0300

    Update

--- common/ui/src/utils/formatter.js
+++ common/ui/src/utils/formatter.js
@@ -10,9 +10,9 @@ export const formatDocument = (value) => {
   return value;
 };
 
-export const formatPhone    = (value) => {
+export const formatPhone = (value) => {
   if (/^([0-9]{10})$/.test(value))
-    return value.replace(/(\d{2})(\d{4})(\d{4})/g,"(\$1) \$2\-\$3");
+    return value.replace(/(\d{2})(\d{4})(\d{4})/g, "(\$1) \$2\-\$3");
 
   if (/^([0-9]{11})$/.test(value))
     return value.replace(/(\d{2})(\d{1})(\d{4})(\d{4})/g, "(\$1) \$2 \$3\-\$4");
@@ -20,22 +20,25 @@ export const formatPhone    = (value) => {
   return value;
 };
 
-export const formatCEP      = (value) => {
+export const formatCEP = (value) => {
   if (value.length == 7)
     value = `0${value}`;
 
   return value
-    .replace(/(\d{5})(\d{3})/g,"\$1\-\$2");
+    .replace(/(\d{5})(\d{3})/g, "\$1\-\$2");
 };
 
-export const formatMoney    = (value, currency, locale) => {
+export const formatMoney = (value, currency, locale) => {
   let formatter = new Intl.NumberFormat(locale, {
-    style   : 'currency',
+    style: 'currency',
     currency: currency,
   });
 
   if (typeof value == 'string')
     value = value.replace(',', '.');
+
+  if (!value)
+    return 0;
 
   return formatter.format(value);
 };
