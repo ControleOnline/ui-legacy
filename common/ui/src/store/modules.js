6cce56a049b4f60d5ca26685ce064353255254ef
Luiz Kim <luiz.kim@controleonline.com>
2022-04-27 10:47:11 -0300

    Update

--- common/ui/src/store/modules.js
+++ common/ui/src/store/modules.js
@@ -1,6 +1,7 @@
 
 import auth from '@controleonline/quasar-login-ui/src/store/modules/auth';
 import quote from '@freteclick/quasar-quote-ui/src/store/modules/quote';
+import coupon from '@freteclick/quasar-coupon-ui/src/store/coupon';
 import salesOrder from "@controleonline/quasar-orders-ui/src/store/modules/order/sales";
 import purchasingOrder from "@controleonline/quasar-orders-ui/src/store/modules/order/purchasing";
 import comissionOrder from "@controleonline/quasar-orders-ui/src/store/modules/order/comission";
@@ -18,6 +19,7 @@ import support from "@controleonline/quasar-support-ui/src/store/support";
 
 export const modules = {
   auth,
+  coupon,
   gmaps,
   people,
   config,
