eee2d7bf958e6caffcd0d3680a6a66ee17cc1482
Luiz Kim <luiz.kim@controleonline.com>
2022-05-25 13:21:43 -0300

    Update

---
+++ import/ui/src/pages/Tasks/Index.vue
@@ -0,0 +1,73 @@
+<template>
+  <q-page padding>
+    <q-card style="min-height: 90vh">
+      <q-card-section>
+        <q-card-section class="text-h5">
+          {{ $t("tasks.title") }}
+        </q-card-section>
+        <div class="row">
+          <div class="col-12">
+            <q-tabs
+              :horizontal="$q.screen.gt.xs"
+              align="justify"
+              v-model="currentTab"
+              class="bg-white text-primary"
+            >
+            <q-tab name="allTasks" :label="$t('tasks.allTasks')" />
+              <q-tab name="myTasks" :label="$t('tasks.myTasks')" />
+              <q-tab name="create" :label="$t('tasks.create')" />
+              <q-tab name="category" :label="$t('tasks.category')" />
+            </q-tabs>
+            <q-separator />
+            <q-tab-panels v-model="currentTab">
+              <q-tab-panel name="allTasks" class="q-px-none">
+                <TasksSearchingAll />
+              </q-tab-panel>
+              <q-tab-panel name="myTasks" class="q-px-none">
+                <MyTasksSearching />
+              </q-tab-panel>
+              <q-tab-panel name="create" class="q-px-none">
+                <TasksSearching />
+              </q-tab-panel>
+              <q-tab-panel name="category" class="q-px-none">
+                <TaskCategorySearching />
+              </q-tab-panel>
+            </q-tab-panels>
+          </div>
+        </div>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import TaskCategorySearching from "../../components/Tasks/TaskCategorySearching";
+import MyTasksSearching from "../../components/Tasks/MyTasksSearching";
+import TasksSearching from "../../components/Tasks/TasksSearching";
+import TasksSearchingAll from "../../components/Tasks/TasksSearchingAll";
+
+import { mapGetters } from "vuex";
+
+export default {
+  name: "TasksIndexPage",
+
+  components: {
+    TaskCategorySearching,
+    MyTasksSearching,
+    TasksSearching,
+    TasksSearchingAll
+  },
+
+  data() {
+    return {
+      currentTab: "myTasks",
+    };
+  },
+
+  computed: {
+    ...mapGetters({
+      user: "auth/user",
+    }),
+  },
+};
+</script>
---
+++ import/ui/src/router/routes.js
@@ -0,0 +1,24 @@
+export const routes = [
+  {
+    path: '/imports/',
+    component: () => import('../../../../../src/layouts/AdminLayout.vue'),
+    children: [
+      {
+        name: 'ImportIndex',
+        path: '',
+        component: () => import('../pages/Import/Index.vue')
+      },
+
+    ]
+  }, {
+    path: '/import/',
+    component: () => import('../../../../../src/layouts/MainLayout.vue'),
+    children: [
+      {
+        name: 'ImportDetails',
+        path: 'id/:id',
+        component: () => import('../pages/Import/Details.vue')
+      }
+    ]
+  },
+];
