2603477e68dc3b9221b6f2753e4e9535746074e5
Luiz Kim <luiz.kim@controleonline.com>
2022-05-26 16:13:18 -0300

    Update

--- import/ui/src/components/Import/ImportsList.vue
+++ import/ui/src/components/Import/ImportsList.vue
@@ -37,7 +37,7 @@
           <q-btn icon="close" flat round dense v-close-popup />
         </q-card-section>
         <q-card-section>
-          <q-uploader ref="uploader" no-thumbnails square flat :url="uploadEndpoint + ',' + tableId"
+          <q-uploader ref="uploader" no-thumbnails square flat :url="uploadEndpoint + '?tableid=' + tableId"
             :headers="uploadHeaders" :accept="uploadAccepted" field-name="file" color="white" @uploaded="fileUploaded"
             @failed="uploadFailed" :multiple="false" :class="myClass" :auto-upload="true">
             <template v-slot:header="scope">
@@ -162,7 +162,7 @@ export default {
         rowsPerPage: 10,
         rowsNumber: 10,
       },
-      uploadEndpoint: `${ENTRYPOINT}/import/delivery_tax`,
+      uploadEndpoint: `${ENTRYPOINT}/import`,
       uploadHeaders: [
         {
           name: 'API-TOKEN',
