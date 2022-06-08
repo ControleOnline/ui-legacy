e1b055df3b21622d8ae9ad62661851681872d9a1
Carlos Mendes <52838023+CarlosMendesDev@users.noreply.github.com>
2022-06-08 02:16:32 -0300

    fix templates trail

--- users/ui/src/pages/User/NotificationsUser.vue
+++ users/ui/src/pages/User/NotificationsUser.vue
@@ -1,51 +1,53 @@
 <template>
-  <MenuDefaultHeader
-    :title="$t('notifications_header')"
-    :save_button="false"
-    icon="o_notifications"
-  >
-  </MenuDefaultHeader>
+  <div>
+    <MenuDefaultHeader
+      :title="$t('notifications_header')"
+      :save_button="false"
+      icon="o_notifications"
+    >
+    </MenuDefaultHeader>
 
-  <q-tabs
-    v-model="tab"
-    no-caps
-    class="text-grey-9 shadow-2 q-px-md "
-  >
-    <q-tab name="all" :label="$t('all')" />
-    <q-tab name="unread" :label="$t('unread')" />
-  </q-tabs>
+    <q-tabs
+      v-model="tab"
+      no-caps
+      class="text-grey-9 shadow-2 q-px-md "
+    >
+      <q-tab name="all" :label="$t('all')" />
+      <q-tab name="unread" :label="$t('unread')" />
+    </q-tabs>
 
-  <q-tab-panels v-model="tab">
-    <q-tab-panel name="all">
-      <CardNotifications
-        v-for="notification in notifications"
-        :key="notification.id"
-        @click="openDialog(notification)"
-        :image_profile="notification.image_profile"
-        :user_name="notification.user_name"
-        :event="notification.event"
-        :action_notification="notification.action_notification"
-        :date="notification.date"
-        :read="notification.read"
-      >
-      </CardNotifications>
-    </q-tab-panel>
+    <q-tab-panels v-model="tab">
+      <q-tab-panel name="all">
+        <CardNotifications
+          v-for="notification in notifications"
+          :key="notification.id"
+          @click="openDialog(notification)"
+          :image_profile="notification.image_profile"
+          :user_name="notification.user_name"
+          :event="notification.event"
+          :action_notification="notification.action_notification"
+          :date="notification.date"
+          :read="notification.read"
+        >
+        </CardNotifications>
+      </q-tab-panel>
 
-    <q-tab-panel name="unread">
-      <CardNotifications
-        v-for="notification in filteredUnreadNotification"
-        :key="notification.id"
-        @click="openDialog(notification)"
-        :image_profile="notification.image_profile"
-        :user_name="notification.user_name"
-        :event="notification.event"
-        :action_notification="notification.action_notification"
-        :date="notification.date"
-        :read="notification.read"
-      >
-      </CardNotifications>
-    </q-tab-panel>
-  </q-tab-panels>
+      <q-tab-panel name="unread">
+        <CardNotifications
+          v-for="notification in filteredUnreadNotification"
+          :key="notification.id"
+          @click="openDialog(notification)"
+          :image_profile="notification.image_profile"
+          :user_name="notification.user_name"
+          :event="notification.event"
+          :action_notification="notification.action_notification"
+          :date="notification.date"
+          :read="notification.read"
+        >
+        </CardNotifications>
+      </q-tab-panel>
+    </q-tab-panels>
+  </div>
 </template>
 
 <script>
