5e6bcf1457578d3022b344824779551bde2206ac
Luiz Kim <luiz.kim@controleonline.com>
2022-06-20 11:11:54 -0300

    Update

--- financial/ui/src/pages/Invoice/Pay/Index.vue
+++ financial/ui/src/pages/Invoice/Pay/Index.vue
@@ -1,67 +1,48 @@
 <template>
   <q-page padding>
-    <q-card
-       style   ="min-height: 90vh;"
-      :bordered="false"
-      :flat    ="true"
-    >
-        <q-card-section class="text-h6">
-          {{ $t('Contas a pagar') }}
-        </q-card-section>
-        <div class="row">
-          <div class="col-12">
-            <q-tabs
-              :horizontal="$q.screen.gt.xs"
-              align      ="justify"
-              v-model    ="currentTab"
-              class      ="bg-white text-primary"
-            >
-              <q-tab
-                name  ="expenses"
-                :label="$t('Despesas')"
-              />
-              <q-tab
-                name  ="categories"
-                :label="$t('Categorias')"
-              />
-            </q-tabs>
+    <q-card style="min-height: 90vh;" :bordered="false" :flat="true">
+      <q-card-section class="text-h6">
+        {{ $t('Contas a pagar') }}
+      </q-card-section>
+      <div class="row">
+        <div class="col-12">
+          <q-tabs :horizontal="$q.screen.gt.xs" align="justify" v-model="currentTab" class="bg-white text-primary">
+            <q-tab name="expenses" :label="$t('Despesas')" />
+            <q-tab name="categories" :label="$t('Categorias')" />
+          </q-tabs>
 
-            <q-separator />
+          <q-separator />
 
-            <q-tab-panels
-              v-model="currentTab"
-            >
-              <q-tab-panel name="expenses" class="q-px-none">
-                <InvoiceSearching />
-              </q-tab-panel>
+          <q-tab-panels v-model="currentTab">
+            <q-tab-panel name="expenses" class="q-px-none">
+              <InvoiceSearching />
+            </q-tab-panel>
 
-              <q-tab-panel name="categories" class="q-px-none">
-                <TableCategories
-                  :api="api"
-                />
-              </q-tab-panel>
-            </q-tab-panels>
-          </div>
+            <q-tab-panel name="categories" class="q-px-none">
+              <TableCategories :context="'expense'" :api="api" />
+            </q-tab-panel>
+          </q-tab-panels>
         </div>
+      </div>
     </q-card>
   </q-page>
 </template>
 
 <script>
-import Api              from '@controleonline/quasar-common-ui/src/utils/api';
+import Api from '@controleonline/quasar-common-ui/src/utils/api';
 import InvoiceSearching from '../../../components/invoice/pay/InvoiceSearching.vue';
-import TableCategories  from '../../../components/expense/TableCategories.vue';
+import TableCategories from '@controleonline/quasar-common-ui/src/components/categories/Table.vue';
 
 export default {
   components: {
-    TableCategories ,
+    TableCategories,
     InvoiceSearching,
   },
 
-  data () {
+  data() {
     return {
       currentTab: 'expenses',
-      api       : null
+      api: null
     }
   },
 
