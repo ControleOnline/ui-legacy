020915c05253078890c4dcf2a048362d77f7365e
Luiz Kim <luiz.kim@controleonline.com>
2022-10-11 12:04:30 -0300

    Update

--- delivery/ui/src/pages/Delivery/List.vue
+++ delivery/ui/src/pages/Delivery/List.vue
@@ -3,22 +3,7 @@
     <q-card style="min-height: 90vh;" :bordered="false" :flat="true">       
       <div class="row">
         <div class="col-12">
-          <q-tabs :horizontal="$q.screen.gt.xs" align="justify" v-model="currentTab" class="bg-white text-primary">
-            <q-tab name="products" :label="$t('Products')" />
-            <q-tab name="categories" :label="$t('Categorias')" />
-          </q-tabs>
-
-          <q-separator />
-
-          <q-tab-panels v-model="currentTab">
-            <q-tab-panel name="products" class="q-px-none">              
-              <ProductList />
-            </q-tab-panel>
-
-            <q-tab-panel name="categories" class="q-px-none">
-              <!--<TableCategories :context="'expense'" :api="api" />-->
-            </q-tab-panel>
-          </q-tab-panels>
+              <OrderList />          
         </div>
       </div>
     </q-card>
@@ -27,12 +12,12 @@
 
 <script>
 import Api from '@controleonline/quasar-common-ui/src/utils/api';
-import ProductList from '../../components/products/ProductList.vue';
+import OrderList from '../../components/products/OrderList.vue';
 
 
 export default {
   components: {
-    ProductList,
+    OrderList,
     //InvoiceSearching,
   },
 
