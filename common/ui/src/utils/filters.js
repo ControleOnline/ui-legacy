55fb146d056fca290f3e36f37cd8cf714de3b512
Luiz Kim <luiz.kim@controleonline.com>
2022-11-14 15:14:35 -0300

    Update

---
+++ common/ui/src/utils/filters.js
@@ -0,0 +1,15 @@
+import { LocalStorage } from "quasar";
+
+export default class Filters {
+  getFilter(router) {
+    if (LocalStorage.has("session")) {
+      let storedUser = LocalStorage.getItem("session");
+      return storedUser.filters[router];
+    }
+  }
+  setFilter(router, value) {
+    let storedUser = LocalStorage.getItem("session");
+    storedUser.filters[router] = value;
+    LocalStorage.set("session", storedUser);
+  }
+}
