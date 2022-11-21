81d621c4e7d54fb9f0979bcca20b4bef8e617550
Luiz Kim <luiz.kim@controleonline.com>
2022-11-21 12:37:46 -0300

    Update

--- common/ui/src/utils/acl.js
+++ common/ui/src/utils/acl.js
@@ -2,41 +2,47 @@ import { LocalStorage } from "quasar";
 import Api from "@controleonline/quasar-common-ui/src/utils/api";
 
 export default class acl {
-  getPermission(action) {    
+  getPermission() {
     let obj = {};
     if (LocalStorage.has("session")) {
-      let storedUser = LocalStorage.getItem("session");      
-      obj = storedUser.actions ? storedUser.actions[storedUser["route"]][action] : {};      
+      let storedUser = LocalStorage.getItem("session");
+      obj = storedUser.actions
+        ? storedUser.actions[storedUser["route"]][storedUser["route"]]
+        : {};
     }
     return obj;
-  };
+  }
   setPermission() {
-    let storedUser = LocalStorage.getItem("session");    
-    let route = storedUser.route;    
+    let storedUser = LocalStorage.getItem("session");
+    let route = storedUser.route;
     if (!storedUser.actions) storedUser.actions = {};
-    //if (!storedUser.actions[route]) storedUser.actions[route] = {};    
     let permissions = this.fetchPermission();
-    
-        storedUser.actions[route] = permissions;
-        LocalStorage.set("session", storedUser);
-       
+    if (permissions != undefined) storedUser.actions[route] = permissions;
+    LocalStorage.set("session", storedUser);
   }
 
-fetchPermission(){
+  fetchPermission() {
     let storedUser = LocalStorage.getItem("session");
     let API = new Api(storedUser.token);
     let route = storedUser.route;
-    console.log(storedUser.actions? Object.keys(storedUser.actions[route]).length:null);
-    if (route && storedUser.mycompany && 
-        (!storedUser.actions || typeof storedUser.actions[route] == undefined)){        
-    return API.private(`acoes/people`, {
-      params: { myCompany: storedUser.mycompany, route: route },
-    }).then((response) => response.json())
-      .then((result) => {        
-        return   result.response?result.response.data:{};        
-      });
+
+    if (
+      route &&
+      storedUser.mycompany &&
+      (!storedUser.actions || storedUser.actions[route] == undefined)
+    ) {
+      return API.private(`acoes/people`, {
+        params: { myCompany: storedUser.mycompany, route: route },
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          return result.response ? result.response.data : {};
+        });
     }
-    return {};
-  
-}
+    return;
+  }
+  hasPermissao(permission) {
+    let p = this.getPermission();
+    return p[permission] ? p[permission] : false;
+  }
 }
