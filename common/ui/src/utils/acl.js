50e66f76a1510cdd28c44a9a93ee7db09288d27e
Luiz Kim <luiz.kim@controleonline.com>
2022-11-21 14:02:50 -0300

    Update

--- common/ui/src/utils/acl.js
+++ common/ui/src/utils/acl.js
@@ -1,48 +1,39 @@
 import { LocalStorage } from "quasar";
-import Api from "@controleonline/quasar-common-ui/src/utils/api";
+import Api from "@nelsys/quasar-common-ui/src/utils/api";
 
 export default class acl {
-  getPermission() {
-    let obj = {};
-    if (LocalStorage.has("session")) {
+  getPermissions() {    
+    if (LocalStorage.has("session")) {      
+      let storedUser = LocalStorage.getItem("session");      
+      return  storedUser.actions ? storedUser.actions[storedUser["route"]] : {};      
+    }
+
+  };
+  setPermission() {         
+   this.fetchPermission();           
+  }
+
+  fetchPermission(){
       let storedUser = LocalStorage.getItem("session");
-      obj = storedUser.actions
-        ? storedUser.actions[storedUser["route"]][storedUser["route"]]
-        : {};
-    }
-    return obj;
-  }
-  setPermission() {
-    let storedUser = LocalStorage.getItem("session");
-    let route = storedUser.route;
-    if (!storedUser.actions) storedUser.actions = {};
-    let permissions = this.fetchPermission();
-    if (permissions != undefined) storedUser.actions[route] = permissions;
-    LocalStorage.set("session", storedUser);
-  }
+      let API = new Api(storedUser.token);
+      let route = storedUser.route;
+      if (!storedUser.actions) storedUser.actions = {};
 
-  fetchPermission() {
-    let storedUser = LocalStorage.getItem("session");
-    let API = new Api(storedUser.token);
-    let route = storedUser.route;
-
-    if (
-      route &&
-      storedUser.mycompany &&
-      (!storedUser.actions || storedUser.actions[route] == undefined)
-    ) {
-      return API.private(`actions/people`, {
+        if (storedUser.mycompany && route)
+      return API.private(`acoes/people`, {
         params: { myCompany: storedUser.mycompany, route: route },
-      })
-        .then((response) => response.json())
-        .then((result) => {
-          return result.response ? result.response.data : {};
-        });
-    }
-    return;
+      }).then((response) => response.json())
+        .then((result) => {        
+          storedUser.actions[route]=  result.response?result.response.data:{};                       
+          LocalStorage.set("session", storedUser);
+        });          
   }
-  hasPermissao(permission) {
-    let p = this.getPermission();
-    return p[permission] ? p[permission] : false;
+
+ hasPermission(permission) {  
+    let permissions = this.getPermissions();  
+    if (Object.values(permissions).indexOf(permission) > -1) {
+      return true;
+    }
+      return false;
   }
 }
