9589a4e221955c9ddbb9fb1b330bd6427b717240
Luiz Kim <luiz.kim@controleonline.com>
2023-01-27 16:06:15 -0300

    Update

---
+++ freight-quote/ui/src/router/routes.js
@@ -0,0 +1,14 @@
+
+export const routes = [
+    {
+        path: '/freight/',
+        component: () => import('../../../../../src/layouts/MainLayout.vue'),
+        children: [
+            {
+                name: 'FreightQuoteIndex',
+                path: 'quote',
+                component: () => import('../pages/FreightQuote.vue')
+            }
+        ]
+    },
+];
