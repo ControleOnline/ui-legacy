dfe014ee0eae198f543fb9a7d736a22eda38958f
Luiz Kim <luiz.kim@controleonline.com>
2023-01-27 17:34:03 -0300

    Update

--- freight/ui/src/utils/api.js
+++ freight/ui/src/utils/api.js
@@ -1,29 +1,30 @@
-import myUrl from 'url';
+import myUrl from "url";
 
 let myStore = null;
 
-export const url = function(id, options = {}) {
-  if (typeof options.headers === 'undefined')
+export const api = function (id, options = {}) {
+  if (typeof options.headers === "undefined")
     Object.assign(options, { headers: new Headers() });
 
-  if (myStore.getters['auth/user'] !== null && myStore.getters['auth/user'].token)
-    options.headers.set(
-      'API-TOKEN', myStore.getters['auth/user'].token
-    );
+  if (
+    myStore.getters["auth/user"] !== null &&
+    myStore.getters["auth/user"].token
+  )
+    options.headers.set("API-TOKEN", myStore.getters["auth/user"].token);
 
   let myPromisse = myUrl(id, options);
 
-  myPromisse.catch(e => {
-    if (e.message == 'Unauthorized' || e.message == 'Invalid credentials.') {
-      myStore.dispatch('auth/logOut');
+  myPromisse.catch((e) => {
+    if (e.message == "Unauthorized" || e.message == "Invalid credentials.") {
+      myStore.dispatch("auth/logOut");
       localStorage.clear();
       location.reload();
     }
   });
 
   return myPromisse;
-}
+};
 
 export default async ({ store }) => {
   myStore = store;
-}
+};
