fb72bca8387fed5e983d0a5827e1441f79b853ee
Luiz Kim <luiz.kim@controleonline.com>
2023-02-06 20:36:56 -0300

    Update

--- queues/ui/src/pages/Hardwares/AllDisplays.vue
+++ queues/ui/src/pages/Hardwares/AllDisplays.vue
@@ -1,5 +1,21 @@
 <template>
-    <q-page padding>
-Displays
-    </q-page>
+  <q-page padding>
+    <div class="row">
+    Sistema de Filas
+    </div>
+    <div class="row">
+    <div class="col-4">Anterior</div>
+    <div class="col-4">Atual</div>
+    <div class="col-4">Próximo</div>
+    </div>
+    <div class="row">
+      <div class="col-4"></div>
+      <div class="col-8">
+        <q-video
+          src="https://www.youtube.com/embed/YriZWDnixWE?rel=0&controls=0&autoplay=true"
+          style="width: 100%px; height: 70vh"
+        />
+      </div>
+    </div>
+  </q-page>
 </template>
