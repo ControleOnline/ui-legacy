065c065b774413f0a43640ea30c8ca9a6c3775fb
Yuri Mansani Vieira <yurimv9@gmail.com>
2023-02-06 07:17:11 -0300

    ajuste redirecionamento e listagem de notificacoes

--- common/ui/src/components/Common/Notifications.vue
+++ common/ui/src/components/Common/Notifications.vue
@@ -1,24 +1,24 @@
 <template>
-  <q-btn round dense flat color="grey-8" icon="notifications">
-      <q-badge v-if="notifications.length > 0" color="red" text-color="white" floating>
-          {{ notifications.length }}
-      </q-badge>
-      <q-tooltip>Notificações</q-tooltip>
-      <q-menu>
-      <template v-for="notfication in notifications ">
-              <q-list style="min-width: 100px">
-                  <q-separator></q-separator>
+    <q-btn round dense flat color="grey-8" icon="notifications">
+        <q-badge v-if="notifications.length > 0" color="red" text-color="white" floating>
+            {{ notificationsUnread }}
+        </q-badge>
+        <q-tooltip>Notificações</q-tooltip>
+        <q-menu>
+            <template v-for="notfication in notifications ">
+                <q-list style="min-width: 100px">
+                    <q-separator></q-separator>
 
-                  <q-item clickable v-close-popup @click="redirecionaTasks(notfication)">
+                    <q-item clickable v-close-popup @click="redirecionaTasks(notfication)">
 
-                      <q-item-section>{{ notfication.id + ' ' + notfication.body }}</q-item-section>
-                  </q-item>
+                        <q-item-section>{{ notfication.id + ' ' + notfication.body }}</q-item-section>
+                    </q-item>
 
-              </q-list>
-          </template>
-      </q-menu>
+                </q-list>
+            </template>
+        </q-menu>
 
-  </q-btn>
+    </q-btn>
 
 </template>
 
@@ -29,58 +29,64 @@ import { mapActions, mapGetters } from "vuex";
 import API from "@controleonline/quasar-common-ui/src/utils/api";
 
 export default {
-  props: {
-      peopleId: {
-          required: true,
-      },
-  },
+    props: {
+        peopleId: {
+            required: true,
+        },
+    },
 
-  data() {
-      return {
-          API: new API(this.$store.getters["auth/user"].token),
-          company: null,
-          notifications: [],
-          notificationsUnread: 0,
-          taskId: '',
-      };
-  },
+    data() {
+        return {
+            API: new API(this.$store.getters["auth/user"].token),
+            company: null,
+            notifications: [],
+            notificationsUnread: 0,
+            taskId: '',
+        };
+    },
 
-  created() {
-      this.getNotifications();
+    created() {
+        this.getNotifications();
 
-  },
+    },
 
-  computed: {
-      ...mapGetters({
-          myCompany: "people/currentCompany",
-      }),
-  },
+    computed: {
+        ...mapGetters({
+            myCompany: "people/currentCompany",
+        }),
+    },
 
-  watch: {
-      myCompany(company) {
-          this.company = company;
-      },
-  },
+    watch: {
+        myCompany(company) {
+            this.company = company;
+        },
+    },
 
-  methods: {
-      ...mapActions({}),
+    methods: {
+        ...mapActions({}),
 
-      getNotifications() {
-          return this.API.private(`/task_interations`, {
-              params: { registeredBy: '/people/' + this.peopleId },
-          })
-              .then((response) => response.json())
-              .then((result) => {
-                  this.notifications =result['hydra:member'];
-              });
-      },
-      redirecionaTasks(task) {
-          // this.$router.push("/tasks/id/" + task.replace('/tasks',''))
-      },
+        getNotifications() {
+            let params = {};
+            params['task.taskFor'] = this.peopleId;
+            params['read'] = 0;
+            params.itemsPerPage = 10;
 
-      click(route) {
-          this.$emit("clickmenu", route);
-      },
-  },
+            return this.API.private(`/task_interations`, {
+                params,
+            })
+                .then((response) => response.json())
+                .then((result) => {
+                    this.notifications = result['hydra:member'];
+                    this.notificationsUnread = result['hydra:totalItems'];
+                });
+        },
+        redirecionaTasks(notification) {
+            this.$router.push("/tasks/id/" + notification.task.replace('/tasks/', ''))
+        },
+
+        click(route) {
+            this.$emit("clickmenu", route);
+        },
+    },
 };
 </script>
