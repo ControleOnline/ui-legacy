7724b6f7a18d30f50d48e8b8dd0dcbf85242b9c2
Luiz Kim <luiz.kim@controleonline.com>
2023-02-07 22:23:35 -0300

    Update

--- queues/ui/src/pages/Hardwares/AllDisplays.vue
+++ queues/ui/src/pages/Hardwares/AllDisplays.vue
@@ -1,21 +1,222 @@
 <template>
-  <q-page padding>
-    <div class="row">
-    Sistema de Filas
-    </div>
-    <div class="row">
-    <div class="col-4">Anterior</div>
-    <div class="col-4">Atual</div>
-    <div class="col-4">Próximo</div>
-    </div>
-    <div class="row">
-      <div class="col-4"></div>
-      <div class="col-8">
-        <q-video
-          src="https://www.youtube.com/embed/YriZWDnixWE?rel=0&controls=0&autoplay=true"
-          style="width: 100%px; height: 70vh"
-        />
+  <q-page>
+    <div class="row full-height-vh">
+      <div class="col-3 q-pa-sm">
+        <q-card class="my-card full-height">
+          <q-card-section>
+            <div class="text-h6">Próximos</div>
+            <div class="text-subtitle">Ordem de gravidade</div>
+            <div class="text-subtitle">Ordem de chegada</div>
+          </q-card-section>
+          <q-separator />
+          <q-list>
+            <q-item>
+              <q-item-section avatar>
+                <q-icon color="primary" name="local_hospital" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>#00001</q-item-label>
+                <q-item-label caption>Ragiologia</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section avatar>
+                <q-icon color="red" name="local_hospital" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>#00002</q-item-label>
+                <q-item-label caption>Clínica Geral</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section avatar>
+                <q-icon color="amber" name="local_hospital" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>#00003</q-item-label>
+                <q-item-label caption>Ambulatório</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item>
+              <q-item-section avatar>
+                <q-icon color="primary" name="local_hospital" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>#00001</q-item-label>
+                <q-item-label caption>Ragiologia</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section avatar>
+                <q-icon color="red" name="local_hospital" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>#00002</q-item-label>
+                <q-item-label caption>Clínica Geral</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section avatar>
+                <q-icon color="amber" name="local_hospital" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>#00003</q-item-label>
+                <q-item-label caption>Ambulatório</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item>
+              <q-item-section avatar>
+                <q-icon color="primary" name="local_hospital" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>#00001</q-item-label>
+                <q-item-label caption>Ragiologia</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section avatar>
+                <q-icon color="red" name="local_hospital" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>#00002</q-item-label>
+                <q-item-label caption>Clínica Geral</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section avatar>
+                <q-icon color="amber" name="local_hospital" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>#00003</q-item-label>
+                <q-item-label caption>Ambulatório</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item>
+              <q-item-section avatar>
+                <q-icon color="primary" name="local_hospital" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>#00001</q-item-label>
+                <q-item-label caption>Ragiologia</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section avatar>
+                <q-icon color="red" name="local_hospital" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>#00002</q-item-label>
+                <q-item-label caption>Clínica Geral</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item>
+              <q-item-section avatar>
+                <q-icon color="amber" name="local_hospital" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>#00003</q-item-label>
+                <q-item-label caption>Ambulatório</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-card>
+      </div>
+
+      <div class="col-9">
+        <div class="row justify-center">
+          <div class="col-sm-4 q-pa-sm">
+            <q-card class="my-card">
+              <q-card-section>
+                <div class="text-h6 text-center">Senha Atual</div>
+              </q-card-section>
+
+              <q-separator />
+              <q-list>
+                <q-item>
+                  <q-item-section avatar>
+                    <q-icon color="primary" name="local_bar" />
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>#00001</q-item-label>
+                    <q-item-label caption>Ambulatório 02</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-card>
+          </div>
+          <div class="col-sm-4 q-pa-sm">
+            <q-card class="my-card">
+              <q-card-section>
+                <div class="text-h6 text-center">Senha Atual</div>
+              </q-card-section>
+
+              <q-separator />
+              <q-list>
+                <q-item>
+                  <q-item-section avatar>
+                    <q-icon color="primary" name="local_bar" />
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>#00001</q-item-label>
+                    <q-item-label caption>Ambulatório 02</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-card>
+          </div>
+          <div class="col-sm-4 q-pa-sm">
+            <q-card class="my-card">
+              <q-card-section>
+                <div class="text-h6 text-center">Senha Atual</div>
+              </q-card-section>
+
+              <q-separator />
+              <q-list>
+                <q-item>
+                  <q-item-section avatar>
+                    <q-icon color="primary" name="local_bar" />
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>#00001</q-item-label>
+                    <q-item-label caption>Ambulatório 02</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-card>
+          </div>
+        </div>
+        <div class="full-width q-pa-sm">
+          <q-video
+            src="https://www.youtube.com/embed/YriZWDnixWE?rel=0&controls=0&autoplay=true"
+            style="width: 100%px; height: 70vh"
+          />
+        </div>
       </div>
     </div>
   </q-page>
 </template>
+<style>
+.full-height-vh {
+  height: calc(100vh - 16px) !important;
+}
+</style>
