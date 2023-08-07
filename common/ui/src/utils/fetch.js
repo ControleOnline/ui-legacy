ef11ff7f52dc77e970044c7c08085308d8dc9c97
Luiz Kim <luiz.kim@controleonline.com>
2023-08-07 12:11:54 -0300

    Update

--- common/ui/src/utils/fetch.js
+++ common/ui/src/utils/fetch.js
@@ -8,98 +8,88 @@ export default function (id, options = {}) {
     ? LocalStorage.getItem("config").language
     : "pt-BR";
 
-  if (options.body) {
-    options.body = JSON.parse(options.body);
-  }
+  const domain = DOMAIN + (DOMAIN.endsWith("/") ? "" : "/");
 
-  if (options.params) {
-    let params = [];
-
-    Object.keys(options.params).map((key) => {
-      if (Array.isArray(options.params[key])) {
-        params.push(
-          options.params[key].map((value) => `${key}[]=${value}`).join("&")
-        );
-      } else {
-        params.push(key + "=" + options.params[key]);
-      }
-    });
-
-    id = `${id}?${params.join("&")}`;
-  }
   const entryPoint =
     id.indexOf("searchBy") != -1 &&
     id.indexOf("/sales/orders") != -1 &&
-    id.indexOf("/detail/status") == -1
+    id.indexOf("/detail/status") == -1 &&
+    domain == "foccuscegonhas.com.br"
       ? "https://api.dev.foccuscegonhas.com.br/"
       : ENTRYPOINT + (ENTRYPOINT.endsWith("/") ? "" : "/");
-  const domain = DOMAIN + (DOMAIN.endsWith("/") ? "" : "/");
-  return fetch(new URL(id, entryPoint), options)
-    .then((response) => {
-      if (response.ok) {
-        return response;
-      }
-      return response
-        .json()
-        .then((json) => {
-          const error = json["hydra:description"]
-            ? json["hydra:description"]
-            : response.statusText;
-          if (!json.violations) {
+
+  return (
+    fetch(new URL(id, entryPoint), options)
+      .then((response) => {
+        if (response.ok) {
+          return response;
+        }
+        return response
+          .json()
+          .then((json) => {
+            const error = json["hydra:description"]
+              ? json["hydra:description"]
+              : response.statusText;
+            if (!json.violations) {
+              Notify.create({
+                message: error,
+                position: "bottom",
+                type: "negative",
+              });
+              throw Error(error);
+            }
+
+            const errors = { _error: error };
+            json.violations.map((violation) =>
+              Object.assign(errors, {
+                [violation.propertyPath]: violation.message,
+              })
+            );
+
             Notify.create({
-              message: error,
+              message: errors,
               position: "bottom",
               type: "negative",
             });
-            throw Error(error);
-          }
-
-          const errors = { _error: error };
-          json.violations.map((violation) =>
-            Object.assign(errors, {
-              [violation.propertyPath]: violation.message,
-            })
-          );
-
+            throw new SubmissionError(errors);
+          })
+          .finally(() => {
+            Loading.hide();
+          });
+      })
+      /*
+      .then((response) => response.json())
+      .then((data) => {
+        let error =
+          data.response && data.response.error
+            ? data.response.error
+            : data.error;
+        if (error) {
           Notify.create({
-            message: errors,
+            message: typeof error == "object" ? error.message : error,
             position: "bottom",
             type: "negative",
           });
-          throw new SubmissionError(errors);
-        })
-        .finally(() => {
-          Loading.hide();
-        });
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      let error =
-        data.response && data.response.error ? data.response.error : data.error;
-      if (error) {
-        Notify.create({
-          message: typeof error == "object" ? error.message : error,
-          position: "bottom",
-          type: "negative",
-        });
-      } else {
-        let method = options ? options.method : null;
-        if (method == "PUT" || method == "POST" || method == "DELETE") {
-          Notify.create({
-            message:
-              messages && messages[lang].actions
-                ? messages[lang].actions[method]
-                  ? messages[lang].actions[method].success
-                  : messages[lang].success
-                : messages[lang].success,
-            position: "bottom",
-            type: "positive",
-          });
+        } else {
+          let method = options ? options.method : null;
+          if (method == "PUT" || method == "POST" || method == "DELETE") {
+            Notify.create({
+              message:
+                messages && messages[lang].actions
+                  ? messages[lang].actions[method]
+                    ? messages[lang].actions[method].success
+                    : messages[lang].success
+                  : messages[lang].success,
+              position: "bottom",
+              type: "positive",
+            });
+          }
         }
-      }
-      return data;
-    })
-    .finally(() => {
-      Loading.hide();
-    });
+        return data;
+      })
+      */
+      .finally(() => {
+        Loading.hide();
+      })
+  );
 }
