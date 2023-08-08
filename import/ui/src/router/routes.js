c47f4479b5947e51cb22fa3e7babb6a11457ae7b
luizkim <luizkim@users.noreply.github.com>
2023-08-07 23:46:58 -0300

    Update routes.js

--- import/ui/src/router/routes.js
+++ import/ui/src/router/routes.js
@@ -4,9 +4,9 @@ export const routes = [
     component: () =>  import ('../../../../../src/layouts/AdminLayout.vue'),
     children: [
       {
-        name: 'importIndex',
+        name: 'ImportIndex',
         path: '',
-        component: () =>  import ('../pages/import/Index.vue')
+        component: () =>  import ('../pages/Import/Index.vue')
       },
 
     ]
@@ -15,9 +15,9 @@ export const routes = [
     component: () =>  import ('../../../../../src/layouts/MainLayout.vue'),
     children: [
       {
-        name: 'importDetails',
+        name: 'ImportDetails',
         path: 'id/:id',
-        component: () =>  import ('../pages/import/Details.vue')
+        component: () =>  import ('../pages/Import/Details.vue')
       }
     ]
   },
