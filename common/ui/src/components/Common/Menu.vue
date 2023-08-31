f16bcbfedcba40073e8797700ebbf7730d867329
Luiz Kim <luiz.kim@controleonline.com>
2023-08-31 11:02:47 -0300

    Update

--- common/ui/src/components/Common/Menu.vue
+++ common/ui/src/components/Common/Menu.vue
@@ -1,23 +1,9 @@
 <template>
   <div>
-    <q-expansion-item
-      expand-icon-toggle
-      :content-inset-level="0.3"
-      :color="mItem.color"
-      :icon="mItem.icon"
-      class="GNL__drawer-item"
-      :label="mItem.label"
-      v-for="mItem in menu.modules"
-      :key="mItem.id"
-    >
-      <q-item
-        v-ripple
-        clickable
-        class="GNL__drawer-item"
-        v-for="item in mItem.menus"
-        :key="item.id"
-        @click="click(item.route)"
-      >
+    <q-expansion-item expand-icon-toggle :content-inset-level="0.3" :color="mItem.color" :icon="mItem.icon"
+      class="GNL__drawer-item" :label="mItem.label" v-for="mItem in menu.modules" :key="mItem.id">
+      <q-item v-ripple clickable class="GNL__drawer-item" v-for="item in mItem.menus" :key="item.id"
+        @click="click(item.route)">
         <q-item-section avatar>
           <q-icon class="item-icon" :name="item.icon" :color="item.color" />
         </q-item-section>
@@ -50,7 +36,7 @@ export default {
     };
   },
 
-  created() {},
+  created() { },
 
   computed: {
     ...mapGetters({
@@ -68,14 +54,52 @@ export default {
   methods: {
     ...mapActions({}),
 
+
+    routeExists(routeName) {
+
+      return this.$router.options.routes.some((route) => {
+        if (route.children)
+          return route.children.some((child) => {
+            return routeName === child.name
+          });
+      });
+    },
     getMenu() {
-      return api
-        .fetch(`menus-people`, {
-          params: { myCompany: this.company.id },
-        })
+      return api.fetch(`menus/people`, {
+        params: { myCompany: this.company.id },
+      })
 
         .then((result) => {
-          this.menu = result.response?.data;
+
+          let menus = result.response?.data?.modules;
+          let modules = [];
+          let index = 0;
+          let naoexiste = true;
+
+          menus.forEach((item, i) => {
+
+            item.menus.forEach((menu, ii) => {
+              if (this.routeExists(menu.rota)) {
+
+                if (!modules[index]) {
+                  modules[index] = item;
+                  modules[index].menus = [];
+                  naoexiste = true;
+                } else {
+                  naoexiste = false;
+                }
+
+                modules[index].menus.push(menu);
+
+                if (!naoexiste)
+                  index++;
+
+
+              }
+            });
+          });
+          menus.modules = modules;
+          this.menu = menus;
         });
     },
 
