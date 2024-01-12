78eaec606534d016e44cfb4a9342c305a8b49444
Luiz Kim <luiz.kim@controleonline.com>
2024-01-11 23:48:10 -0300

    Update

--- quasar-default-ui/src/components/Default/DefaultSearch.vue
+++ quasar-default-ui/src/components/Default/DefaultSearch.vue
@@ -1,5 +1,5 @@
 <template>
-        <q-input class="q-pa-xs" v-if="configs.search != false" borderless dense @keyup.enter="onSearch" v-model="search"
+    <q-input class="q-pa-xs" v-if="configs.search != false" borderless dense @keyup.enter="onSearch" v-model="search"
         :placeholder="$t('Search')">
         <template v-slot:append>
             <q-btn flat icon="search" @click="onSearch"></q-btn>
@@ -15,7 +15,7 @@ export default {
             type: Object,
             required: true,
         },
-    },    
+    },
     computed: {
         columns() {
             return this.copyObject(this.$store.getters[this.configs.store + '/columns'])
@@ -23,6 +23,9 @@ export default {
         filters() {
             return this.$store.getters[this.configs.store + '/filters']
         },
+    },
+    created() {
+        this.search = this.copyObject(this.filters).search;
     },
     data() {
         return {
