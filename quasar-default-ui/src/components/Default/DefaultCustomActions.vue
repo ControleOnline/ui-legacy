e837dd8a0221505049d7530d794490a33f15955f
Luiz Kim <luiz.kim@controleonline.com>
2024-02-21 12:30:05 -0300

    Update

--- quasar-default-ui/src/components/Default/DefaultCustomActions.vue
+++ quasar-default-ui/src/components/Default/DefaultCustomActions.vue
@@ -9,7 +9,7 @@
               <q-icon size="sm" :name="itm.icon"></q-icon>
             </q-item-section>
             <q-item-section>
-              {{ $translate(itm.store, itm.title, 'menu') }}
+              {{ $translate(itm.store || configs.store, itm.title, 'menu') }}
             </q-item-section>
           </q-item>
         </template>
@@ -27,7 +27,11 @@ export default {
   emits: ['loadData'],
   props: {
     componentProps: Object,
-    row: Object
+    row: Object,
+    configs: {
+      type: Object,
+      required: false
+    }
   },
 
   data() {
