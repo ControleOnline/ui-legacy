bb6200f7e40fa7f4d18927a427edbf6ebe387368
Luiz Kim <luiz.kim@controleonline.com>
2024-03-03 23:02:32 -0300

    Update Home

--- quasar-layout-ui/src/pages/Home/index.vue
+++ quasar-layout-ui/src/pages/Home/index.vue
@@ -1,69 +1,64 @@
 <template>
     <q-list>
-      <q-expansion-item
-        :content-inset-level="0.3"
-        :icon="mItem.icon"
-        :label="mItem.label"
-        v-for="mItem in menus"
-        :key="mItem.id"
-        default-opened
-      >
-        <q-card v-for="(item, index) in mItem.menus" :key="index" class="icon-card"  @click="click(item)">
-          <q-card-section class="q-pa-md">
-            <q-avatar class="big-icon">
-              <q-icon :name="item.icon" size="50px" />
-            </q-avatar>
-          </q-card-section>
-          <q-card-section class="q-pa-md">
-            <div class="text-center">{{ $t(item.label) }}</div>
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
+        <q-expansion-item :content-inset-level="0.3" :icon="mItem.icon" :label="mItem.label" v-for="mItem in menus"
+            :key="mItem.id" default-opened>
+            <q-card v-for="(item, index) in mItem.menus" :key="index" class="icon-card" @click="click(item)">
+                <q-card-section class="q-pa-md">
+                    <q-avatar class="big-icon">
+                        <q-icon :name="item.icon" size="50px" />
+                    </q-avatar>
+                </q-card-section>
+                <q-card-section class="q-pa-md">
+                    <div class="text-center">{{ $t(item.label) }}</div>
+                </q-card-section>
+            </q-card>
+        </q-expansion-item>
     </q-list>
-  </template>
+</template>
   
-  <script>
-  import { mapActions, mapGetters } from "vuex";
-  
-  export default {
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
     data() {
-      return {};
+        return {};
     },
-  
+
     computed: {
-      ...mapGetters({
-        myCompany: "people/currentCompany",
-      }),
-      menus() {
-        return this.$copyObject(this.$store.getters["theme/menus"]);
-      },
+        ...mapGetters({
+            myCompany: "people/currentCompany",
+        }),
+        menus() {
+            return this.$copyObject(this.$store.getters["theme/menus"]);
+        },
     },
     created() {
-      console.log(this.menus);
+
     },
     methods: {
-      ...mapActions({}),
-      routeExists(routeName) {
-        return this.$router.options.routes.some((route) => {
-          if (route.children) return route.children.some((child) => routeName === child.name);
-        });
-      },
-      click(route) {
-        this.$emit("clickmenu", route);
-        this.$router.push({ name: route.route });
-      },
+        ...mapActions({}),
+        routeExists(routeName) {
+            return this.$router.options.routes.some((route) => {
+                if (route.children) return route.children.some((child) => routeName === child.name);
+            });
+        },
+        click(route) {
+            this.$emit("clickmenu", route);
+            this.$router.push({ name: route.route });
+        },
     },
-  };
-  </script>
+};
+</script>
   
-  <style scoped>
-  .icon-card {
-    width: 45%; /* Adjust the width of the cards as needed */
+<style scoped>
+.icon-card {
+    width: 45%;
+    /* Adjust the width of the cards as needed */
     margin: 10px;
-  }
-  
-  .big-icon {
+}
+
+.big-icon {
     margin: auto;
-  }
-  </style>
+}
+</style>
   
