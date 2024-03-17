c6f162ec93f264ac96c70b4aae4a9d25110fb9c7
Luiz Kim <luiz.kim@controleonline.com>
2024-03-17 18:52:36 -0300

    Update

--- quasar-layout-ui/src/pages/Home/index.vue
+++ quasar-layout-ui/src/pages/Home/index.vue
@@ -1,64 +1,93 @@
 <template>
-    <q-list>
-        <q-expansion-item :content-inset-level="0.3" :icon="mItem.icon" :label="mItem.label" v-for="mItem in menus"
-            :key="mItem.id" default-opened>
-            <q-card v-for="(item, index) in mItem.menus" :key="index" class="icon-card" @click="click(item)">
-                <q-card-section class="q-pa-md">
-                    <q-avatar class="big-icon">
-                        <q-icon :name="item.icon" size="50px" />
-                    </q-avatar>
-                </q-card-section>
-                <q-card-section class="q-pa-md">
-                    <div class="text-center">{{ $t(item.label) }}</div>
-                </q-card-section>
-            </q-card>
-        </q-expansion-item>
-    </q-list>
+  <q-list>
+    <q-expansion-item
+      :content-inset-level="0.3"
+      class="home-menu"
+      :icon="mItem.icon"
+      :label="mItem.label"
+      v-for="(mItem, mIndex) in menus"
+      :key="mItem.id"
+      default-opened
+    >
+      <div class="row q-col-gutter-md">
+        <q-card
+          class="icon-card col-md-3 col-sm-6 col-lg-2 col-xl-1 col-sm-6 col-xs-12 d-flex flex-wrap justify-between q-ma-md"
+          v-for="(item, index) in mItem.menus"
+          :key="index"
+          @click="click(item)"
+          :style="{
+            backgroundColor: getCardBackground(
+              mIndex.toString() + index.toString()
+            ),
+          }"
+        >
+          <q-card-section class="q-pa-md">
+            <q-avatar class="big-icon">
+              <q-icon :name="item.icon" size="50px" />
+            </q-avatar>
+          </q-card-section>
+          <q-card-section class="q-pa-md">
+            <div class="text-center">{{ $t(item.label) }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </q-expansion-item>
+  </q-list>
 </template>
-  
+
 <script>
 import { mapActions, mapGetters } from "vuex";
 
 export default {
-    data() {
-        return {};
-    },
+  data() {
+    return {};
+  },
 
-    computed: {
-        ...mapGetters({
-            myCompany: "people/currentCompany",
-        }),
-        menus() {
-            return this.$copyObject(this.$store.getters["theme/menus"]);
-        },
+  computed: {
+    ...mapGetters({
+      myCompany: "people/currentCompany",
+    }),
+    menus() {
+      return this.$copyObject(this.$store.getters["theme/menus"]);
     },
-    created() {
-
+  },
+  created() {},
+  methods: {
+    ...mapActions({}),
+    routeExists(routeName) {
+      return this.$router.options.routes.some((route) => {
+        if (route.children)
+          return route.children.some((child) => routeName === child.name);
+      });
     },
-    methods: {
-        ...mapActions({}),
-        routeExists(routeName) {
-            return this.$router.options.routes.some((route) => {
-                if (route.children) return route.children.some((child) => routeName === child.name);
-            });
-        },
-        click(route) {
-            this.$emit("clickmenu", route);
-            this.$router.push({ name: route.route });
-        },
+    click(route) {
+      this.$emit("clickmenu", route);
+      this.$router.push({ name: route.route });
     },
+    getCardBackground(index) {
+      const colors = [
+        "#ff6f61", // Vermelho
+        "#6b5b95", // Roxo
+        "#88b04b", // Verde
+        "#f8b500", // Amarelo
+        "#4a4e4d", // Cinza
+        "#003f5c", // Azul Escuro
+      ];
+      return colors[parseInt(index) % colors.length];
+    },
+  },
 };
 </script>
-  
+
 <style scoped>
 .icon-card {
-    width: 45%;
-    /* Adjust the width of the cards as needed */
-    margin: 10px;
+  cursor: pointer;
+}
+.home-menu {
+  width: 100%;
 }
 
 .big-icon {
-    margin: auto;
+  margin: auto;
 }
 </style>
-  
