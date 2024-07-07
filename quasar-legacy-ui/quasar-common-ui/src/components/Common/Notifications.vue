00760849da5ed31dd9346b9aa5ca5c9a1d119bee
Luiz Kim <luiz.kim@controleonline.com>
2024-07-06 23:39:07 -0300

    Socket!!!

--- quasar-legacy-ui/quasar-common-ui/src/components/Common/Notifications.vue
+++ quasar-legacy-ui/quasar-common-ui/src/components/Common/Notifications.vue
@@ -1,6 +1,11 @@
 <template>
   <q-btn round dense flat color="grey-8" icon="notifications">
-    <q-badge v-if="notifications.length > 0" color="red" text-color="white" floating>
+    <q-badge
+      v-if="notifications.length > 0"
+      color="red"
+      text-color="white"
+      floating
+    >
       {{ notificationsUnread }}
     </q-badge>
     <q-tooltip>Notificações</q-tooltip>
@@ -8,12 +13,23 @@
       <template v-for="notfication in notifications">
         <q-list style="min-width: 100px">
           <q-separator></q-separator>
-
-          <q-item clickable v-close-popup @click="redirecionaTasks(notfication)">
+          <q-item
+            clickable
+            v-close-popup
+            @click="redirecionaTasks(notfication)"
+          >
             <q-item-section>{{ notfication.notification }}</q-item-section>
           </q-item>
         </q-list>
       </template>
+      <div>
+        <h6>Mensagens do WebSocket</h6>
+        <ul>
+          <li v-for="(message, index) in messages" :key="index">
+            {{ message }}
+          </li>
+        </ul>
+      </div>
     </q-menu>
   </q-btn>
 </template>
@@ -21,6 +37,7 @@
 <script>
 import { api } from "@controleonline/../../src/boot/api";
 import { mapActions, mapGetters } from "vuex";
+import io from "socket.io-client";
 
 export default {
   data() {
@@ -28,12 +45,16 @@ export default {
       company: null,
       notifications: [],
       notificationsUnread: 0,
+      reconnectInterval: 5000, // Intervalo de reconexão em milissegundos
       taskId: "",
+      socket: null,
+      messages: [],
     };
   },
 
   created() {
     this.getNotifications();
+    this.socketInit();
   },
 
   computed: {
@@ -47,13 +68,45 @@ export default {
       this.company = company;
     },
   },
-
+  beforeDestroy() {
+    if (this.socket) {
+      this.socket.close();
+    }
+  },
   methods: {
     ...mapActions({}),
+    socketInit() {
+      // Conectar ao servidor WebSocket
+      this.socket = new WebSocket("wss://ws.controleonline.com");
 
+      // Evento de conexão aberta
+      this.socket.onopen = () => {
+        console.log("Conectado ao servidor WebSocket");
+      };
+
+      // Evento de mensagem recebida
+      this.socket.onmessage = (event) => {
+        this.messages.push(event.data);
+      };
+
+      // Evento de erro
+      this.socket.onerror = (error) => {
+        console.error("WebSocket error:", error);
+      };
+
+      // Evento de conexão fechada
+      this.socket.onclose = () => {
+        console.log(
+          "Conexão com WebSocket fechada. Tentando reconectar em " +
+            this.reconnectInterval / 1000 +
+            " segundos..."
+        );
+        setTimeout(this.connectWebSocket, this.reconnectInterval);
+      };
+    },
     getNotifications() {
       let params = {};
-      params["people.id"] = this.$store.getters['auth/user'].people;
+      params["people.id"] = this.$store.getters["auth/user"].people;
       params["read"] = 0;
       params.itemsPerPage = 10;
 
@@ -68,7 +121,10 @@ export default {
         });
     },
     redirecionaTasks(notfication) {
-      this.$router.push({ name: notfication.route, params: { id: notfication.routeId } });
+      this.$router.push({
+        name: notfication.route,
+        params: { id: notfication.routeId },
+      });
     },
 
     click(route) {
