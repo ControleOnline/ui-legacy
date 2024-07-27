d2ea47943cad61a3f700b725c6f47c066f32f279
Rodrigo Valentoni Peach <rvalentoni@hotmail.com>
2024-07-26 22:15:35 -0300

    commited

---
+++ publish.sh
@@ -0,0 +1,165 @@
+cd accounting
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd carrier
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd common
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd config
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd contracts
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd coupon
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd crm
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd customers
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd dashboard
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd delivery
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd docs
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd financial
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd freight
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd guides
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd import
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd layout
+cd ui
+npm publish --access=public
+
+cd login
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd orders
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd people
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd products
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd professionals
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd providers
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd queues
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd quote
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd shop
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd support
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd tasks
+cd ui
+npm publish --access=public
+cd ..
+cd ..
+
+cd users
+cd ui
+npm publish --access=public
+cd ..
+cd ..
