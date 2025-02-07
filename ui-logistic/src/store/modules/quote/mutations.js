f16abeb0aec0ee0373c5c23936c446cff0c4a3cc
Luiz Kim <luiz.kim@controleonline.com>
2025-02-07 14:10:01 -0300

    Redux

--- ui-logistic/src/store/modules/quote/mutations.js
+++ ui-logistic/src/store/modules/quote/mutations.js
@@ -1,23 +1,28 @@
-import * as types from './mutation_types';
+import * as types from "./mutation_types";
 
 export default {
-  [types.SET_ERROR]     (state, error) {
-    Object.assign(state, { error });
+  [types.SET_ERROR](state, payload) {
+    if (!payload?.error) Object.assign(state, { error: payload });
+    return { ...state, error: payload?.error || payload };
   },
 
-  [types.SET_ISLOADING] (state, isLoading = true) {
-    Object.assign(state, { isLoading: isLoading });
+  [types.SET_ISLOADING](state, payload = true) {
+    if (!payload?.isLoading) Object.assign(state, { isLoading: payload });
+    return { ...state, isLoading: payload?.isLoading || payload };
   },
 
-  [types.SET_VIOLATIONS](state, violations) {
-    Object.assign(state, { violations });
+  [types.SET_VIOLATIONS](state, payload) {
+    if (!payload?.violations) Object.assign(state, { violations: payload });
+    return { ...state, violations: payload?.violations || payload };
   },
 
-  [types.SET_RETRIEVED] (state, retrieved) {
-    Object.assign(state, { retrieved });
+  [types.SET_RETRIEVED](state, payload) {
+    if (!payload?.retrieved) Object.assign(state, { retrieved: payload });
+    return { ...state, retrieved: payload?.retrieved || payload };
   },
 
-  [types.SET_SHOW_TAXES](state, showTaxes) {
-    Object.assign(state, { showTaxes });
+  [types.SET_SHOW_TAXES](state, payload) {
+    if (!payload?.showTaxes) Object.assign(state, { showTaxes: payload });
+    return { ...state, showTaxes: payload?.showTaxes || payload };
   },
 };
